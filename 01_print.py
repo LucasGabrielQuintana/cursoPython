aa5fc2c384f4457140cb789a3ee275f14db21859
Lucas Quintana <66920069+LucasGabrielQuintana@users.noreply.github.com>
2023-06-26 05:45:41 -0300

    Add files via upload
    Comenzando con Print y variables

---
+++ 01_print.py
@@ -0,0 +1,22 @@
+print("Hola, esto es Python")
+print("Hola, soy Lucas")
+
+#Operaciones matematicas
+print(12+5)
+
+print(12-5)
+
+print(2*3)
+print(8/2)
+
+#Esto es un comentario.
+
+"""
+Comentario de 
+varias lineas
+"""
+
+'''
+Comentario 
+de varias lineas
+'''
---
+++ 02_vars.py
@@ -0,0 +1,19 @@
+print("Hola vars")
+
+#Variable 
+my_name = "Lucas"
+print(my_name)
+
+#Variable con numero
+my_age = 34
+print(my_age)
+
+my_name = "Gabriel"
+print("nombre cambiado", my_name)
+
+#input
+my_name = input("Cual es tu nombre?\n")
+
+print("Usando input", my_name)
+
+my_age = input("Cual es tu edad?\n")
