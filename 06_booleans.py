e2151d10822a518e032f24b1b122ec7a274916e6
Lucas Quintana <66920069+LucasGabrielQuintana@users.noreply.github.com>
2023-06-27 06:51:45 -0300

    booleans and transformations

---
+++ 06_booleans.py
@@ -0,0 +1,8 @@
+is_single = True
+print(type(is_single)) # bool
+print(is_single)
+
+print(not(is_single)) # El operador "not" invierte el valor
+
+is_single = not is_single
+print(is_single)
---
+++ 07_transformation.py
@@ -0,0 +1,24 @@
+name = "Lucas"
+print(type(name))
+name = 12
+print(type(name))
+name = True 
+print(type(name))
+
+# Python toma los tipos de valores en forma dinamica
+
+print("Lucas " + "Gabriel")
+print(10+20)
+
+print("Lucas" + "1")
+
+age = 34
+
+print("Mi edad es " + str(age))
+
+print(f"Mi edad es {age}")
+
+age = input('Escribe tu edad => ')
+print(type(age))
+age = int(age)
+print(f"Tu edad en 10 años será {age +10}")
